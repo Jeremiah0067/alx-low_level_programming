797a02efb58270d597f5f607c7c65b1287a1caf5
Jeremiah Okolo <okolojeremiah2022@gmail.com>
2022-12-13 14:09:12 -0800

    nesting

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- wonderfully made
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+        putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- Just needed
+ *
+ * Return: always returns zero
+ */
+
+int _putchar(char c)
+{
+	putchar(c);
+
+	return (0);
+}
